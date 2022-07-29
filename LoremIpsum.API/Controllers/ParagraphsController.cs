ff07a4b4def80111a836c04521c50143d2093273
Sergio Pistone <spistone@quadiontech.com>
2022-07-29 15:24:48 -0300

    Added LoremIpsum.API project and solution.

---
+++ LoremIpsum.API/Controllers/ParagraphsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLipsum.Core;
+
+namespace LoremIpsum.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ParagraphsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{count}")]
+        public IEnumerable<string> Get(uint count)
+        {
+            var generator = new LipsumGenerator();
+            return generator.GenerateParagraphs((int)count, new Paragraph(1, 10));
+        }
+    }
+}
---
+++ LoremIpsum.API/Controllers/SentencesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLipsum.Core;
+
+namespace LoremIpsum.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SentencesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("sentences/{count}")]
+        public IEnumerable<string> Get(uint count)
+        {
+            var generator = new LipsumGenerator();
+            return generator.GenerateSentences((int)count, new Sentence(20, 40));
+        }
+    }
+}
